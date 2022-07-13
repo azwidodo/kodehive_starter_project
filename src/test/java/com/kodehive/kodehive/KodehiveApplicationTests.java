aaf2a55d110e9c9c61de9af3b5051f6cc89a9973
azwidodo <61899073+azwidodo@users.noreply.github.com>
2022-07-13 10:07:16 +0700

    initial commit

---
+++ src/test/java/com/kodehive/kodehive/KodehiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodehive.kodehive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodehiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
