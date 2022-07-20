ac5472032e2e712d1022933cd77b88f15c8496fd
azwidodo <61899073+azwidodo@users.noreply.github.com>
2022-07-20 16:29:15 +0700

    js quiz

--- src/main/java/com/kodehive/kodehive/controller/MahasiswaController.java
+++ src/main/java/com/kodehive/kodehive/controller/MahasiswaController.java
@@ -2,11 +2,20 @@ package com.kodehive.kodehive.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @RequestMapping("/kodehive")
 public class MahasiswaController {
+
+	@RequestMapping("/home")
+	public String home() {
+		return "/home";
+	}
+
+	@RequestMapping("/account")
+	public String account() {
+		return "/account";
+	}
 
 	@RequestMapping("/karyawan")
 	public String viewKaryawan() {
---
+++ src/main/resources/static/js/learn.js
@@ -0,0 +1,43 @@
+var angka = [1, 2, 3, 4]
+
+// angka.forEach(item => { console.log(item * item) })
+// same thing as:
+// angka.forEach(function (item) { console.log(item * item) })
+
+var buku = { judul: "Spring in Action", harga: 360979, penulis: "Craig Walls" }
+
+console.log(buku)
+
+for (item in buku) {
+    console.log(item + ": " + buku[item])
+}
+
+console.log(isNaN("False"))
+
+function awesomeFunction() {
+    var a = 2
+    var multiplyBy2 = function () {
+        console.log(a * 2)
+    }
+}
+
+var grand_total = eval("10 * 20 / 2")
+
+console.log(typeof (grand_total))
+
+// var myHobby = {
+//     hobby: "football",
+//     func: function () {
+//         var self = this;
+//         document.write("outer func: this.hobby = " + this.hobby + "<br/>")
+//         document.write("outer func: this.hobby = " + self.hobby + "<br/>")
+
+//             (function () {
+//                 document.write("inner func: this.hobby = " + this.hobby + "<br/>")
+//                 document.write("inner func: this.hobby = " + self.hobby)
+//             }())
+
+//     }
+// }
+
+// myHobby.func()
